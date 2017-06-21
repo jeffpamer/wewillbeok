1ca181059385cc49579c644488a2e3c5e0bcfc27
Jeff Pamer <jeff@mediumdivision.com>
2017-06-20 17:53:53 -0700

    Move analytics inside React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-32335417-3');
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
