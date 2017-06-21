084ce57d1d6a0bddafd9a79749fa418358a1ffcc
Jeff Pamer <jeff@mediumdivision.com>
2017-06-20 17:00:59 -0700

    Revert to loading screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// import LoadingIndicator from './LoadingIndicator';
-import TestDrawing2 from './TestDrawing2';
+import LoadingIndicator from './LoadingIndicator';
+// import TestDrawing2 from './TestDrawing2';
 
-const App = () => (
+const App = () =>
   <div>
-    {/*<LoadingIndicator text="we will be ok" />*/}
-    <TestDrawing2 />
-  </div>
-);
+    <LoadingIndicator text="we will be ok" />
+    {/* <TestDrawing2 /> */}
+  </div>;
 
 export default App;
